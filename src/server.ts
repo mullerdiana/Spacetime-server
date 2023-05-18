ecdaac5f0403321f95908bf472884a6512c3ec11
Diana Müller <diana.muller@globalhitss.com.br>
2023-05-18 15:15:52 -0300

    Memories CRUD

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true, // todas as URLs de front-end poderão acessar nosso back-end
 })
+app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running on http:///localhost:3333')
